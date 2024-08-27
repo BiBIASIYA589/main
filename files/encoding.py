a50e3c13fddb6fbd2e7affe1bcfb9c4807547781
hafsa987 <149224993+hafsa987@users.noreply.github.com>
2024-08-27 09:59:40 +0530

    Delete files directory

--- files/encoding.py
+++
@@ -1,8 +0,0 @@
-def string_to_ascii(input_string):
-    ascii_codes = [ord(char) for char in input_string]
-    return ascii_codes
-
-# Example usage
-input_string = "githubtoken"
-ascii_codes = string_to_ascii(input_string)
-print(ascii_codes)
